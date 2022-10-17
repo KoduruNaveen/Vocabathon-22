d53677bfab6ff683c545d0e18fc353e7106261a9
KoduruNaveen <115937498+KoduruNaveen@users.noreply.github.com>
2022-10-17 22:31:55 +0530

    Create Vocabulary.cpp

---
+++ Vocabulary.cpp
@@ -0,0 +1,30 @@
+"1":{
+    "word": "Angel ",
+    "definitions": [
+        "a spiritual being believed to act as an attendant, agent, or messenger of God"
+    ],
+    "parts-of-speech": "Verb"
+},
+
+"2";{
+      " word": "Beautiful " ,
+      " definitions" : [
+          " of a very high standard; excellent"
+      ],
+      "parts of speech": "Verb"
+},
+
+"3";{
+      " word": " Loyal" ,
+      " definitions" : [
+          "giving or showing firm and constant support  "
+      ],
+      "parts of speech": " Adjective"
+},
+ "4";{
+      " word": " Honest " ,
+      " definitions" : [
+          "used to persuade someone of the truth of something.  "
+      ],
+      "parts of speech": " Adjective "
+},
